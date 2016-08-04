57ac6558a695fbaafe0c47bdfe04fe3c9dca1673
kanekotic <alvarojosepl@gmail.com>
2016-08-04 13:30:18 +0200

    creating html sections

--- server/server.js
+++ server/server.js
@@ -8,11 +8,13 @@ var isDevMode = process.argv.includes('--dev');
 if(isDevMode)
     require('./webpack-load')(app);
 
+app.use(express.static('client/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 app.get('/', function (req, res) {
-    res.sendFile(path.resolve(__dirname + '/../client/index.html'));
+    res.sendFile(path.resolve(__dirname + '/../client/views/index.html'));
 });
 
 app.listen(3000);
