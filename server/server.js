0a6980188b9f946fe89b9994487ccc59b9263d6f
kanekotic <alvarojosepl@gmail.com>
2016-08-08 14:10:20 +0200

    added end points

--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ var path = require('path');
 var app = express();
 var bodyParser = require('body-parser');
 
+var airlines = require('./api/airlines.js');
+var airports = require('./api/airports.js');
+var search = require('./api/search.js');
+
+
 var isDevMode = process.argv.includes('--dev');
 
 if(isDevMode)
@@ -12,6 +17,9 @@ app.use(express.static('client/views/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/airlines', airlines);
+app.use('/airports', airports);
+app.use('/search', airports);
 
 app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname + '/../client/views/index.html'));
