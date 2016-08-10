6c8a8256326a8960578910015010ab821557b388
kanekotic <alvarojosepl@gmail.com>
2016-08-10 16:22:26 +0200

    improve code readibility

--- server/api/search.js
+++ server/api/search.js
@@ -3,26 +3,27 @@ var airlines = require('./requests/airlines.js');
 var search = require('./requests/search.js');
 var router = express.Router();
 
-router.post('/', function(req, res) {
-    var searchPromise = new Promise((resolve, reject)=>{
+function SearchAllAirlines(req){
+    return new Promise((resolve, reject)=>{
         airlines().then((airlinesResult) => {
             var results = [];
             var promises = [];
 
-            for (var airline of JSON.parse(airlinesResult)) {
+            for (var airline of JSON.parse(airlinesResult))
                 promises.push(search(req.body, airline.code));
-            }
 
             var all = Promise.all(promises).then(result => {
                 for (var res of result)
                     results = results.concat(JSON.parse(res)); 
             });
 
-            all.then(() => resolve(results)).catch(error => reject('error'));
+            all.then(() => resolve(results));
         });
     });
+}
 
-    searchPromise.then((result) => {res.send(result);}).catch((error) => {res.send(error);});
+router.post('/', function(req, res) {
+    SearchAllAirlines(req).then((result) => {res.send(result);}).catch((error) => {res.send(error);});
 });
 
 module.exports = router;
