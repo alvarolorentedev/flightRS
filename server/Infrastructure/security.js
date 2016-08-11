206995c632c03949bd475bbcc03de39607ae71a6
kanekotic <alvarojosepl@gmail.com>
2016-08-11 13:39:44 +0200

    address session security

--- server/Infrastructure/security.js
+++ server/Infrastructure/security.js
@@ -1,6 +1,8 @@
 var helmet = require('helmet');
+var session = require('express-session');
 
 function security(app){
+    app.set('trust proxy', 1);  
     app.use(helmet());
     app.use(function(req,res,next){
         res.header("Cache-Control", "no-cache, no-store, must-revalidate");
@@ -10,6 +12,12 @@ function security(app){
 
         next();
     });
+    app.use(session({
+        secret: 'not a secret',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { httpOnly:true, secure:true }
+    }));
 }
 
 module.exports = security;
