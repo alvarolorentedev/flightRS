bc7f328368063dec2cf9daab950bae8fe513d8a8
kanekotic <alvarojosepl@gmail.com>
2016-08-11 09:34:43 +0200

    removed fake data

--- client/api-handler.js
+++ client/api-handler.js
@@ -1,36 +1,3 @@
-var mockFlights = {
-                day2D : 
-                [
-                    {airline: "AF", departs: new Date(), arrives: new Date(), price: "200 $"},
-                    {airline: "KLM", departs: new Date(), arrives: new Date(), price: "170 $"},
-                    {airline: "BA", departs: new Date(), arrives: new Date(), price: "270 $"}
-                ],
-                day1D : 
-                [
-                    {airline: "AF", departs: new Date(), arrives: new Date(), price: "200 $"},
-                    {airline: "Iberia", departs: new Date(), arrives: new Date(), price: "170 $"},
-                    {airline: "BA", departs: new Date(), arrives: new Date(), price: "270 $"}
-                ],
-                day0 : 
-                [
-                    {airline: "Air France", departs: new Date(), arrives: new Date(), price: "200 $"},
-                    {airline: "KLM", departs: new Date(), arrives: new Date(), price: "170 $"},
-                    {airline: "BA", departs: new Date(), arrives: new Date(), price: "270 $"}
-                ],
-                day1U : 
-                [
-                    {airline: "Air France", departs: new Date(), arrives: new Date(), price: "200 $"},
-                    {airline: "KLM", departs: new Date(), arrives: new Date(), price: "170 $"},
-                    {airline: "BA", departs: new Date(), arrives: new Date(), price: "270 $"}
-                ],
-                day2U : 
-                [
-                    {airline: "Air France", departs: new Date(), arrives: new Date(), price: "200 $"},
-                    {airline: "KLM", departs: new Date(), arrives: new Date(), price: "170 $"},
-                    {airline: "BA", departs: new Date(), arrives: new Date(), price: "270 $"}
-                ]
-            };
-
 module.exports = class apiHandler{
     constructor(aggregator){
         this._aggregator = aggregator;
